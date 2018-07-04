91a0c9367b5aca2778713dc8f66be7cb56d99e56
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-07-04 17:46:36 +0800

    update

---
+++ 008.variables.go
@@ -0,0 +1,15 @@
+/*
+    var 用于声明一个变量列表, 跟函数的参数列表一样，类型在最后。
+    var 语句可以出现在包或函数级别。
+*/
+
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+    var i int
+    fmt.Println(i, c, python, java)
+}
