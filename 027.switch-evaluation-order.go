1eef16d8176b863143304bee36d21c8697948402
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-07-06 10:22:19 +0800

    update

--- 027.switch-evaluation-order.go
+++ 027.switch-evaluation-order.go
@@ -12,4 +12,14 @@ import (
 func main() {
     fmt.Println("When's Saturday?")
     today := time.Now().Weekday()
+    switch time.Saturday {
+    case today:
+        fmt.Println("Today.")
+    case today + 1:
+        fmt.Println("Tomorrow.")
+    case today + 2:
+        fmt.Println("In two days.")
+    default:
+        fmt.Println("Too far away.")
+    }
 }
