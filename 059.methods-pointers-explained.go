4a395855e3f57b24c35b9da835b64fea5ca88ec4
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-07-09 12:32:45 +0800

    update

---
+++ 059.methods-pointers-explained.go
@@ -0,0 +1,30 @@
+/*
+    指针与函数
+    我们把 Abs 和 Scale 方法重写为函数
+*/
+
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type Vertex struct {
+    X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+    return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale(v *Vertex, f float64) {
+    v.X = v.X * f
+    v.Y = v.Y * f
+}
+
+func main() {
+    v := Vertex{3, 4}
+    Scale(&v, 10)
+    fmt.Println(Abs(v))
+}
