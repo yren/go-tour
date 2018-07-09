69115b3b6bc1a393a24099ad19ada83e5d6ae804
Yufei Ren [@yufei.ren] <yufei.ren@thomsonreuters.com>
2018-07-09 17:17:58 +0800

    update

--- 077.range-and-close.go
+++ 077.range-and-close.go
@@ -7,3 +7,26 @@
     
     循环 for i := range c 会不断从信道接收值，知道它被关闭。
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
